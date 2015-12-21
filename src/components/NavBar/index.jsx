807f1c5930920ffaa52db39e4f16c2c93ba22ec8
Joe Lencioni <joe.lencioni@gmail.com>
2015-12-21 15:43:23 -0800

    Link up "logo"
    This begins to lay the foundation for our basic app navigation.

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './index.scss';
+import { Link } from 'react-router';
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <div className={styles.component}>
-        <div>
-          Logo
-        </div>
+        <Link to="/">
+          Gift Thing
+        </Link>
+
         <div>
           Log in
         </div>
