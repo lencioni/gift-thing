1d50eddf8c59d6fc49babe502c551cf1167a3580
Joe Lencioni <joe.lencioni@gmail.com>
2015-12-22 18:08:28 -0800

    Add log in link to logged out home
    This will help people log in.

--- src/views/HomeView/index.jsx
+++ src/views/HomeView/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import './index.scss';
 
 export default class HomeView extends React.Component {
   render() {
     return (
       <div className="container text-center">
-        <h1>Welcome to the React Redux Starter Kit</h1>
+        <a href ="/auth/login/facebook">Log in</a>
       </div>
     );
   }
